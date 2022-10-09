2c3f23c05b1dc32ed1b60483fbd8abbd49e4e7f9
user <user@DESKTOP-AUTRPJD>
2022-10-09 23:52:17 +0500

    Add project files.

---
+++ Method2CalSal/EmpChild.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2CalSal
+{
+    public abstract class Employee
+    {
+        // attributes
+        protected double baseSal = 15000;
+        protected double fuelAllowance;
+        protected double medAllowance;
+
+        // actions
+        
+        public double GetBaseSal()
+        {
+            return baseSal;
+        }
+        public double GetFuelAllowance()
+        {
+            return fuelAllowance;
+        }
+
+        public double GetMedAllowance()
+        {
+            return medAllowance;
+        }
+
+        public abstract double CalSal();
+
+    }
+
+    public class Engineer : Employee
+    {
+        public Engineer(double fuelAllowance, double medAllowance)
+        {
+            this.fuelAllowance = fuelAllowance;
+            this.medAllowance = medAllowance;
+
+        }
+        public override double CalSal()
+        {
+         
+            double annualPaid = 7000;
+            double salary = GetBaseSal() + GetFuelAllowance() + GetMedAllowance() + annualPaid / 12;
+            return salary;
+        }
+
+    }
+    public class Manager : Employee
+    {
+        double tax = 0.17;
+        public Manager(double fuelAllowance, double medAllowance)
+        {
+            this.fuelAllowance = fuelAllowance;
+            this.medAllowance = medAllowance;
+
+        }
+        public override double CalSal()
+        {
+            double salary = GetBaseSal() + GetFuelAllowance() + GetMedAllowance();
+            salary = salary - salary * tax;
+            return salary;
+        }
+
+    }
+    public class HR : Employee
+    {
+        public HR(double fuelAllowance, double medAllowance)
+        {
+            this.fuelAllowance = fuelAllowance;
+            this.medAllowance = medAllowance;
+
+        }
+        public override double CalSal()
+        {
+            double intrwAllowance = (GetMedAllowance() + GetFuelAllowance()) * 0.10;
+            double salary = GetBaseSal() + GetFuelAllowance() + GetMedAllowance() + intrwAllowance;
+            return salary;
+        }
+
+    }
+
+}
---
+++ Method2CalSal/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2CalSal
+{
+    internal class Program
+    {
+        // hussain make sure you can use static here or not
+        static Employee GetObj(string _profession)
+        {
+            if (_profession == "Manager")
+            {
+                return new Manager(250, 1000);
+            }
+            else if (_profession == "Engineer")
+            {
+                return new Engineer(0, 0);
+            }
+            else
+            {
+                return new HR(150, 800);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To get check salary kindly enter you proffesion___");
+            Console.WriteLine("\t 1. Engineer");
+            Console.WriteLine("\t 2. Manager");
+            Console.WriteLine("\t 3. HR");
+            Console.Write("\n \t  =");
+            string profession = Console.ReadLine();
+
+            Employee employee = GetObj(profession);
+
+            Console.WriteLine("\n \t \t \t your salary is " + employee.CalSal());
+            
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+
+}
